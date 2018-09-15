644e7778f037cf7e7be32a07cb627ba6d8a6cdea
MarkHalle <43143850+MarkHalle@users.noreply.github.com>
2018-09-15 14:06:24 -0500

    Camera Zoom and Movement Functionality
    Added a zoom function with the mousewheel.
    Added movement in x/y axis with right mouse button.

---
+++ Molecular-Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Camera myCamera;
+    private float distanceZoom = -10;
+    private float movementX = 0;
+    private float movementY = 0;
+    //private Vector3 offset;
+    //private Vector3 orgin;
+
+    // Use this for initialization
+    void Start () {
+        myCamera = transform.GetComponent<Camera>();
+        //offset = transform.position - orgin;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HandleZoom();
+        Vector3 orgin = Vector3.zero;
+        //distance = Math.Sqrt(myCamera)
+        //bool leftMouseClick = Input.GetButton("Mouse 0");
+        //transform.position = orgin + offset;
+    }
+
+    private void HandleZoom() {
+        float ScrollMovement = Input.GetAxis("Mouse ScrollWheel");
+        if (ScrollMovement < 0)
+        {
+            distanceZoom--;
+        }
+        if (ScrollMovement > 0)
+        {
+            distanceZoom++;
+        }
+        bool rightMouseClick = Input.GetButton("Mouse 1");
+        if (rightMouseClick)
+        {
+            movementX = Input.GetAxis("Mouse X") + movementX;
+            movementY = Input.GetAxis("Mouse Y") + movementY;
+        }
+        Vector3 cameraZoom = new Vector3(movementX, movementY, distanceZoom);
+        myCamera.transform.position = cameraZoom;
+    }
+}
