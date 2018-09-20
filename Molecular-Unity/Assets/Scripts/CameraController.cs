9b1c55d6edd5c23067729a36b01181a54c20fd0d
MarkHalle <43143850+MarkHalle@users.noreply.github.com>
2018-09-20 11:38:02 -0500

    Updated camera code

--- Molecular-Unity/Assets/Scripts/CameraController.cs
+++ Molecular-Unity/Assets/Scripts/CameraController.cs
@@ -9,22 +9,16 @@ public class CameraController : MonoBehaviour {
     private float distanceZoom = -10;
     private float movementX = 0;
     private float movementY = 0;
-    //private Vector3 offset;
-    //private Vector3 orgin;
+
 
     // Use this for initialization
     void Start () {
         myCamera = transform.GetComponent<Camera>();
-        //offset = transform.position - orgin;
     }
 	
 	// Update is called once per frame
 	void Update () {
         HandleZoom();
-        Vector3 orgin = Vector3.zero;
-        //distance = Math.Sqrt(myCamera)
-        //bool leftMouseClick = Input.GetButton("Mouse 0");
-        //transform.position = orgin + offset;
     }
 
     private void HandleZoom() {
@@ -37,6 +31,7 @@ public class CameraController : MonoBehaviour {
         {
             distanceZoom++;
         }
+        
         bool rightMouseClick = Input.GetButton("Mouse 1");
         if (rightMouseClick)
         {
@@ -46,4 +41,5 @@ public class CameraController : MonoBehaviour {
         Vector3 cameraZoom = new Vector3(movementX, movementY, distanceZoom);
         myCamera.transform.position = cameraZoom;
     }
+
 }
