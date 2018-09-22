6e824772465e6db95841c59970f7b411fb4e1c2e
MarkHalle <43143850+MarkHalle@users.noreply.github.com>
2018-09-22 13:57:07 -0500

    Updated prefabs
    -removed script from bond prefab

--- Molecular-Unity/Assets/Scripts/Magnetic.cs
+++ Molecular-Unity/Assets/Scripts/Magnetic.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class Magnetic : MonoBehaviour {
 
-    public Vector3 newPosition;
+    public Collider bondTop;
+    public Collider bondBottom;
+    public Collider element;
+    public GameObject target;
     private Transform trans;
     public float magneticSpeed = 1;
 
 	// Use this for initialization
 	void Start () {
         trans = transform;
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        trans.position = Vector3.Lerp(trans.position, newPosition, Time.deltaTime * magneticSpeed);
-        if (Mathf.Abs(newPosition.x - trans.position.x) < 0.05)
+        //trans.position = Vector3.Lerp(trans.position, element.transform.position, Time.deltaTime * magneticSpeed);
+        if (bondTop.bounds != element.bounds)
         {
-            trans.position = newPosition;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, magneticSpeed);
         }
-		
 	}
 }
