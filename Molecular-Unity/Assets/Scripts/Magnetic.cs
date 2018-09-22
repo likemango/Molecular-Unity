12a81b78274b2a9ff20b8d63cfa061792d262d2b
MarkHalle <43143850+MarkHalle@users.noreply.github.com>
2018-09-22 11:51:28 -0500

    Prefabs for element and bond
    Added prefab for an element
    Added prefab for a bond
    WIP Magnetic forces between them

---
+++ Molecular-Unity/Assets/Scripts/Magnetic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnetic : MonoBehaviour {
+
+    public Vector3 newPosition;
+    private Transform trans;
+    public float magneticSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+        trans = transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        trans.position = Vector3.Lerp(trans.position, newPosition, Time.deltaTime * magneticSpeed);
+        if (Mathf.Abs(newPosition.x - trans.position.x) < 0.05)
+        {
+            trans.position = newPosition;
+        }
+		
+	}
+}
