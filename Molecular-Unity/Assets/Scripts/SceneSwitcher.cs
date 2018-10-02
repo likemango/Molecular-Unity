f43e55d3bc07ed60b25cc3b203510a4038d44a44
Quinton Dean <dean.quinton@ymail.com>
2018-10-01 21:37:06 -0500

    Adding in UI elements, scene switching and connect play to molecule view

---
+++ Molecular-Unity/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+    // Work in progress
+    // TODO: Unsure if Unity cleans up assets when SceneManager.LoadScene is called
+
+    // This class will hold the scenes we need to switch to and from
+    public void MoleculeView()
+    {
+        Debug.Log("Switch to Molecule View");
+        SceneManager.LoadScene(1);
+    }
+
+    // Quit the application.
+    public void Quit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+}
